5bfc96d5929f8dca37e2f84d95dc758a6be75d41
Lenin Santiago <lnnsntg@gmail.com>
2021-10-10 06:30:45 +0200

    Agregada funcion borrar nota

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,44 +1,56 @@
-const fs = require('fs')
+const fs = require("fs");
 
 const getNotes = function () {
-    return 'Your notes...'
-}
+  return "Your notes...";
+};
 
 const addNote = function (title, body) {
-    const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
 
-    if (duplicateNotes.length === 0) {
-        notes.push({
-            title: title,
-            body: body
-        })
-        saveNotes(notes)
-        console.log('New note added!')
-    } else {
-        console.log('Note title taken!')
-    }
-}
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log("New note added!");
+  } else {
+    console.log("Note title taken!");
+  }
+};
 
 const saveNotes = function (notes) {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJSON)
-}
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
 
 const loadNotes = function () {
-    try {
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        console.log(JSON.parse(dataJSON));
-        return JSON.parse(dataJSON)
-    } catch (e) {
-        return []
-    }
-}
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+//-----------------------------------------------------------------------
+
+const removeNote = function (title) {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(function (note) {
+    return note.title !== title;
+  });
+  console.log(notesToKeep);
+  saveNotes(notesToKeep);
+  console.log("Note removed!");
+};
+//-----------------------------------------------------------------------
 
 module.exports = {
-    getNotes: getNotes,
-    addNote: addNote
-}
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+};

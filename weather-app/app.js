968d105a25a3e6b3aae034ae785b92638034f008
Lenin Santiago <lnnsntg@gmail.com>
2021-10-15 18:09:16 +0200

    Añadido app.js a weather-app y request npm-pack

---
+++ weather-app/app.js
@@ -0,0 +1,33 @@
+const request = require('request');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Current Weather API Endpoint
+
+// http://api.weatherstack.com/current
+//     ? access_key = YOUR_ACCESS_KEY
+//     & query = New York
+    
+// optional parameters: 
+
+    // & units = m
+    // & language = en
+    // & callback = MY_CALLBACK 

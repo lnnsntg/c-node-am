b75388f6a289433fcb20ffd4e7c32165d40cf06f
Lenin Santiago <lnnsntg@gmail.com>
2021-10-31 01:32:29 +0200

    Anadido partials a templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,31 +1,46 @@
 const path = require("path");
 const express = require("express");
-const app = express();
 const hbs = require("hbs")
+const app = express();
 const port = 3000;
 
 //Define paht for Express config
 const publicDirectoryPaht = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //Setup handlebars engine and views locations
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPaht));
 
-app.get("", (req, res) => {
+//------------------------------------------------------------------------
+
+app.get("/", (req, res) => {
   res.render("index", {
     title: "Weather App",
     name: "Lenin Santiago",
   });
 });
 
+//------------------------------------------------------------------------
+
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "Acerca de mi",
     name: "Lenin Santiago",
+  });
+});
+
+//------------------------------------------------------------------------
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "HELP",
+    helpText: "Aqui va el texto del parrafó de ayuda",
   });
 });
 

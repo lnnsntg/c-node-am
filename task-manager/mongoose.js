eeb7f6f9e87107754e4ccea8f783e1de2389f18d
Lenin Santiago <lnnsntg@gmail.com>
2021-11-18 17:12:10 +0100

    Nuevos cambios en mongoose

--- task-manager/mongoose.js
+++ task-manager/mongoose.js
@@ -3,16 +3,20 @@ const validator = require('validator');
 
 //-------------------------------------------------------------
 
+/* 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
   email: {
     type: String,
     required: true,
+    trim: true,
+    lowercase: true,
     validate(value) {
-      if (!validator.isEmail(value)) {
+      if (validator.isEmail(value) === false) {
         throw new Error('Email is not valid')
       }
     }
@@ -20,17 +24,25 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    trim: true,
+    minlength: 8,
     validate(value) {
       if (validator.isEmpty(value)) {
         throw new Error('You must provide a password')
+      }
+      if(value.includes('password')){
+        throw new Error("The password can not contain 'password'")
+      }
+      if (!validator.isLength(value, { min: 8 })) {
+        throw new Error("The password must have a minimum of 6 characters")
       }
     }
   },
   age: {
     type: Number,
-    required: true,
+    default: 0,
     validate(value) {
-      if (value <= 0) {
+      if (value < 0) {
         throw new Error('Age must be a positive number')
       }
     }
@@ -47,6 +59,11 @@ async function main() {
     console.log("Connection to database failed");
     return "failure"
   }
+  const me = new User({
+    name: "Lenin",
+    email: "lenin@lenin.app",
+    password: 'ppasswordd',
+  });
 
   await me.save()
     .then((d) => console.log(d))
@@ -55,45 +72,51 @@ async function main() {
   await mongoose.disconnect()
     .then(() => console.log('Disconnect'))
 }
-//main()
-
+main()
+ */
 //-------------------------------------------------------------
+const taskSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3
+  },
+  completed: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+const Task = mongoose.model('Task', taskSchema)
 
-// const Tasks = mongoose.model("Tasks", {
-//   description: {
-//     type: String
-//   },
-//   completed: {
-//     type: Boolean
-//   }
-// })
+const task = new Task({
+  description: "Rere"
+})
 
-// const task = new Tasks({
-//   description: "Go to shower",
-//   completed: false
-// });
+async function main() {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/task-manager-api");
+    console.log("Connect to database");
+  } catch (error) {
+    console.log("Connection to database failed");
+    return "failure"
+  }
+  await task.save()
+    .then((d) => console.log(d))
+    .then(() => console.log("Done.!"))
+    .catch((error) => console.log('The fields could not be saved', error.message))
 
-// main().then(console.log)
+  await mongoose.disconnect()
+    .then(() => console.log('Disconnect'))
+  return 'End'
+}
+main().then(console.log)
 
-// async function main() {
-//   try {
-//     await mongoose.connect("mongodb://localhost:27017/task-manager-api");
-//     console.log("Connect to database");
-//   } catch (error) {
-//     console.log("Connection to database failed");
-//     return "failure"
-//   }
 
-//   try {
-//     await task.save()
-//       .then((d) => console.log(d))
-//       .then(() => console.log("Done.!"))
-//   } catch (error) {
-//     (error) => console.log('The fields could not be saved', error)
-//   }
-//   finally {
-//     await mongoose.disconnect()
-//       .then(() => console.log('Disconnect'))
-//     return 'End'
-//   }
-// }
+// await me.save()
+// .then((d) => console.log(d))
+// .catch((error) => console.log('The fields could not be saved', error.message))
+
+// await mongoose.disconnect()
+// .then(() => console.log('Disconnect'))

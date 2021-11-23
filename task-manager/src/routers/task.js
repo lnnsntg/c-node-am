57ffcd4fc98c03188786ade66498374faef470c5
Lenin Santiago <lnnsntg@gmail.com>
2021-11-23 19:16:21 +0100

    añadi router for express

---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const { connect, disconnect } = require("../db/mongoose");
+const Task = require("../models/task");
+const router = new express.Router()
+
+router.post("/tasks", async (req, res) => {
+    const task = new Task(req.body);
+    await connect();
+    await task
+        .save()
+        .then(() => {
+            res.send(task);
+        })
+        .catch((error) => {
+            res.status(400).send(error.message);
+        });
+    await disconnect();
+});
+
+//----------------------------------------------------------
+
+router.get("/tasks", async (req, res) => {
+    await connect();
+    await Task.find()
+        .then((d) => {
+            res.send(d);
+        })
+        .catch((error) => {
+            res.status(500).send(error.message);
+        });
+    await disconnect();
+});
+
+//----------------------------------------------------------
+
+router.get("/tasks/:id", async (req, res) => {
+    const _id = req.params.id;
+    await connect();
+    await Task.findById(_id)
+        .then((task) => {
+            if (!task) {
+                return res.status(404).send();
+            }
+            res.send(task);
+        })
+        .catch((error) => {
+            res.status(404).send(error.message);
+        });
+    await disconnect();
+});
+
+//----------------------------------------------------------
+
+router.patch("/tasks/:id", async (req, res) => {
+    await connect();
+    const updates = Object.keys(req.body);
+    const allowedUpdate = ['description', 'completed'];
+    const isValidOperacion = updates.every((item) => allowedUpdate.includes(item));
+    if (!isValidOperacion) {
+        return res.status(404).send({ error: 'Invalid updates!' });
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+    disconnect();
+});
+
+//----------------------------------------------------------
+
+router.delete('/tasks/:id', async (req, res) => {
+    const id = req.params.id;
+    await connect();
+
+    await Task.findOneAndDelete({ _id: id })
+        .then((task) => {
+            if (!task) {
+                return res.status(404).send();
+            }
+            res.send(task);
+            console.log(task);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+    await disconnect();
+});
+
+/* 
+router.delete('/tasks/:id', async (req, res) => {
+    const id = req.params.id;
+    await connect();
+    try {
+        const task = await Task.findOneAndDelete({ _id: id });
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task);
+        console.log(task);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+    await disconnect();
+});
+ */
+
+
+
+module.exports = router
---
+++ task-manager/src/routers/user.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const { connect, disconnect } = require("../db/mongoose");
+const User = require("../models/user");
+const router = new express.Router();
+
+//----------------------------------------------------------
+
+router.post("/users", async (req, res) => {
+    const user = new User(req.body);
+    await connect();
+    await user
+        .save()
+        .then(() => {
+            res.status(201).send(user);
+        })
+        .catch((error) => {
+            res.status(400).send(error.message);
+        });
+    await disconnect();
+});
+
+//----------------------------------------------------------
+
+router.get("/users", async (req, res) => {
+    await connect();
+    await User.find()
+        .then((d) => {
+            res.send(d);
+        })
+        .catch((error) => {
+            res.status(500).send(error.message);
+        });
+    await disconnect();
+});
+
+//----------------------------------------------------------
+
+router.get("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+    await connect();
+    await User.findById(_id)
+        .then((user) => {
+            res.send(user);
+            console.log(user);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+    await disconnect();
+});
+
+//----------------------------------------------------------
+
+router.patch("/users/:id", async (req, res) => {
+    await connect();
+    const updates = Object.keys(req.body);
+    console.log(updates);
+    const allowedUpdate = ['name', 'email', 'password', 'age'];
+    const isValidOperacion = updates.every((updates) => {
+        return allowedUpdate.includes(updates);
+    });
+    console.log(isValidOperacion);
+    if (!isValidOperacion) {
+        return res.status(404).send({ error: 'Invalid updates!' });
+    }
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+    disconnect();
+});
+
+//----------------------------------------------------------
+
+router.delete('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    await connect();
+    await User.findOneAndDelete({ _id: id })
+        .then((user) => {
+            res.send(user);
+            console.log(user);
+        })
+        .catch((error) => {
+            res.status(500).send(error);
+        });
+    await disconnect();
+});
+
+
+module.exports = router;
